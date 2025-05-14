d14048d40ab8dd9fac6855aec8ec58c88ebc6e0c
Лада <yarkova.lada@gmail.com>
2025-05-14 20:36:17 +0300

    correct css and js

--- hw-3.js
+++ hw-3.js
@@ -28,24 +28,24 @@ alert(Number(a) + Number(b));
 // Задание 5
 let monthNumber = Number(prompt('Введите номер месяца'));
 switch (monthNumber) {
-    case 1:
-    case 2:
-    case 3:        
+    case 3:
+    case 4:
+    case 5:        
         console.log('Весна')
         break;
-    case 4:
-    case 5:
-    case 6:        
+    case 6:
+    case 7:
+    case 8:        
         console.log('Лето')
         break;
-    case 7:
-    case 8:
-    case 9:        
+    case 9:
+    case 10:
+    case 11:        
         console.log('Осень')
         break;
-    case 10:
-    case 11:
-    case 12:        
+    case 12:
+    case 1:
+    case 2:        
         console.log('Зима')
         break;
     default:
