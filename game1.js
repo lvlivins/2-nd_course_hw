d87cae60219acaee3ea019cb0f2f9230cb3cf1b5
Лада <yarkova.lada@gmail.com>
2025-07-23 20:59:10 +0300

    change game1

--- game1.js
+++ game1.js
@@ -3,14 +3,13 @@ const userAnswer = document.getElementById('userAnswer');
 const winText = document.querySelector('.games-box__win-txt');
 const noWinText = document.querySelector('.games-box__nowin-txt');
 
-
 // +1 диапазон становится 1 до 100, а не от 0 до 100 
 // Math.random() — генерирует случайное число от 0 до 1 
 // parseInt() - преобразует строку в целое число / убирает дробь
 
-checkNumber.onclick = function () {
+const secretNumber = parseInt(Math.random() * 100) + 1;
 
-    const secretNumber = parseInt(Math.random() * 100) + 1;
+checkNumber.onclick = function () {
     const number = Number(userAnswer.value);
 
     if (isNaN(number) || number < 1 || number > 100) {
@@ -19,7 +18,7 @@ checkNumber.onclick = function () {
 
     if (number === secretNumber) {
         winText.style.display = 'block';
-        noWinText.style.display = 'none'; 
+        noWinText.style.display = 'none';
         return;
     }
     else {
@@ -37,20 +36,20 @@ checkNumber.onclick = function () {
 console.log('Загаданное число:', secretNumber);
 
 
-// Угадывается только одно заданное число 
+// Угадывается только одно заданное число
 // checkNumber.onclick = function () {
 
 //     winText.style.display = 'none';
 //     noWinText.style.display = 'none';
 //     // каждая попытка ввода будет убирать отображение текста, чтобы не оставалось
 
-//     const number = Number(userAnswer.value); 
-//     для получения ответа в textarea и преобразовании его в число 
-//     .value - спец свойство у элемента ввода (input, textarea), где хранится то, что ввёл пользователь. 
-//     .textContent - для обычных элементов 
+//     const number = Number(userAnswer.value);
+//     для получения ответа в textarea и преобразовании его в число
+//     .value - спец свойство у элемента ввода (input, textarea), где хранится то, что ввёл пользователь.
+//     .textContent - для обычных элементов
 //     .innerHTML — если нужно HTML-содержимое
 
-//     if (Number.isNaN(number)) { 
+//     if (Number.isNaN(number)) {
 //         Number.isNaN()
 //         alert('Введите число!');
 //         return;
@@ -70,5 +69,5 @@ console.log('Загаданное число:', secretNumber);
 //         } else {
 //             alert('Число меньше 47');
 //         }
-//     } 
+//     }
 // };
